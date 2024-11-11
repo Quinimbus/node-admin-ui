bbc26319c75554c7a4ce9d9978a910b7f35dff62
Marcel Witte <marcel@aliquis.de>
2024-11-11 01:28:24 +0100

    fixed SelectionField

--- src/components/form/SelectionField.vue
+++ src/components/form/SelectionField.vue
@@ -12,16 +12,20 @@ defineProps({
 const emit = defineEmits<{
     'update:model-value': [modelValue: string]
 }>()
+const updateValue = (value: string) => {
+    emit('update:model-value', value)
+}
 </script>
 
 <template>
     <div class="flex flex-col flex-wrap gap-4">
         <label>{{ field.label }}</label>
-        <div v-for="(allowedValue, index) in field.allowedValues" class="flex items-center">
+        <div v-for="(allowedValue, index) in field.allowedValues" class="flex items-center gap-2">
             <RadioButton
-                :id="field.key + '_' + index"
+                :inputId="field.key + '_' + index"
                 :value="allowedValue.key"
-                :model-value="modelValue" />
+                :model-value="modelValue"
+                @update:model-value="updateValue" />
             <label :for="field.key + '_' + index">{{ allowedValue.label }}</label>
         </div>
     </div>
